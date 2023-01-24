25e5fbb14df5be4439547a5938222431aecd7992
Abdul Haidar <abdulhdr1@gmail.com>
2023-01-23 22:30:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Svelte-Kit + Vite
+
+https://coolors.co/palette/f8f9fa-e9ecef-dee2e6-ced4da-adb5bd-6c757d-495057-343a40-212529
+This template should help get you started developing with Tauri and Svelte-Kit in Vite.
+
+## Recommended IDE Setup
+
+[VS Code](https://code.visualstudio.com/) + [Svelte](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer).
---
+++ src/lib/Operand.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { stack } from "../stores/stack";
+	import { css } from "@emotion/css";
+
+	export let operand = "";
+	let stackValue = "";
+
+	stack.subscribe((v) => (stackValue = v));
+
+	function appendStack() {
+		if (stackValue.at(-1)?.match(/\d/)) {
+			stack.update((p) => `${p}${operand}`);
+		} else {
+			const newStack = stackValue.slice(0, stackValue.length - 1);
+			stack.update((p) => `${p}${operand}`);
+		}
+	}
+</script>
+
+<button
+	class={css`
+		background-color: green;
+		displat: block;
+		width: 100%;
+	`}
+	on:click={appendStack}>{operand}</button
+>
